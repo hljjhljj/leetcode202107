862fb8f9c89899f88b540b8fddae52793a543421
hljjhljj <56719064+hljjhljj@users.noreply.github.com>
2022-01-20 19:26:57 +0800

    Update 剑指 Offer 38. 字符串的排列.cpp

--- 回溯算法/剑指 Offer 38. 字符串的排列.cpp
+++ 回溯算法/剑指 Offer 38. 字符串的排列.cpp
@@ -1,3 +1,4 @@
+//用set去重有点无赖
 class Solution {
 public:
     set<string> res;
@@ -15,6 +16,33 @@ public:
         }
         for(int i=0;i<s.size();i++){
             if(used[i]==true) continue;
+            temp+=s[i];
+            used[i]=true;
+            myfunction(temp,s,used);
+            temp=temp.substr(0,temp.size()-1);
+            used[i]=false;
+
+        }
+    }
+};
+
+//和全排列II类似，可以看那个
+class Solution {
+public:
+    vector<string> res;
+    vector<string> permutation(string s) {
+        vector<bool> used(s.size(),false);
+        sort(s.begin(),s.end());
+        myfunction("",s,used);
+        return res;
+    }
+    void myfunction(string temp,string s,vector<bool>& used){
+        if(temp.size()==s.size()){
+            res.push_back(temp);
+            return;
+        }
+        for(int i=0;i<s.size();i++){
+            if(used[i] || i-1>=0 && s[i-1]==s[i] && !used[i-1]) continue;
             temp+=s[i];
             used[i]=true;
             myfunction(temp,s,used);
