92c9aa46cd7ce0b9cb9fd9566c36739eeb87edea
hljjhljj <56719064+hljjhljj@users.noreply.github.com>
2022-02-22 20:15:18 +0800

    Update and rename 20. 有效的括号 to 20. 有效的括号.cpp

--- 其他/20. 有效的括号.cpp
+++ 其他/20. 有效的括号.cpp
@@ -8,7 +8,7 @@ public:
             //左括号全部压栈
             if(c=='(' || c=='[' || c=='{') stack.push(c);
             //接下来要pop，防止栈为空，也就是输入为']'这种情况
-            if(stack.empty()) return false;
+            else if(stack.empty()) return false;
             else if(c==')')
                 if(stack.top()=='(') stack.pop();
                 else return false;
