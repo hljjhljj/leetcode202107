059e0f4b9e7e7ce4be158e66f605479fa0bbcf10
hljjhljj <56719064+hljjhljj@users.noreply.github.com>
2022-02-23 10:46:22 +0800

    Update and rename 69. x 的平方根 to 69. x 的平方根.cpp

--- 其他/69. x 的平方根.cpp
+++ 其他/69. x 的平方根.cpp
@@ -13,3 +13,18 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left=0,right=x,res;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if((long long)mid*mid == x) return mid;
+            if((long long)mid*mid < x) left=mid+1;
+            else right=mid-1;
+        }
+        return right;
+    }
+};
